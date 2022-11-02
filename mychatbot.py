1967cd8046bcf69bd973d943c2c6e234ca05267f
vamado09 <108496966+vamado09@users.noreply.github.com>
2022-11-02 13:46:50 -0400

    Add files via upload

---
+++ mychatbot.py
@@ -0,0 +1,20 @@
+# Basic Chatbot using Python and Flask
+# Vicente De Leon
+
+from flask import Flask, render_template, request
+from chat import Chat_T
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('main.html');
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    chat = Chat_T()
+    return str(chat.chat(userText))
+
+if __name__ == "__main__":
+    app.run()
